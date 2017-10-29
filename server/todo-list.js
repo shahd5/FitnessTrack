201cfb87f6c86f519b922b840357cbc689812204
Unknown <shahd5@hawkmail.newpaltz.edu>
2017-10-29 18:04:35 -0400

    server setup

---
+++ server/todo-list.js
@@ -0,0 +1,15 @@
+module.exports = function(app){
+    app.get('/todo', function(req,res){
+        res.render('ok');
+    });
+
+    app.post('/todo', function(req,res){
+        
+    });
+
+    app.delete('/todo', function(req,res){
+        
+    });
+
+    
+};
